9f27228348d433909f9e5b70f8d44bf656b60dd2
Bipin Manandhar <kubipin07@gmail.com>
2026-05-12 12:32:43 +0545

    Initial commit for Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/main.css'],
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+  tailwindcss: {
+    configPath: '~/tailwind.config.ts',
+  },
+})
